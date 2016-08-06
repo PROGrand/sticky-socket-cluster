7a23e43a6d69577985cee7f921a8d77d55988100
Vladimir E. Koltunov <PROGrand@users.noreply.github.com>
2016-08-06 15:32:12 +0300

    Create test.js

---
+++ test/test.js
@@ -0,0 +1,40 @@
+require('sticky-socket-cluster/replace-console')();
+    // prefixes console output with worker ids.
+
+var options = {
+    workers: 2, // total workers (default: cpu cores count).
+    first_port: 8000, // 8000, 8001 are worker's ports (default: 8000).
+    proxy_port: 5000, // default (5000).
+    session_hash: function (req, res) { return req.connection.remoteAddress; },
+        // can use cookie-based session ids and etc. (default: int31 hash).
+
+    no_sockets: false // allow socket.io proxy (default: false).
+};
+
+require('sticky-socket-cluster')(options, start);
+
+function start(port) {
+	const path = require('path');
+	const cookieParser = require('cookie-parser');
+	const bodyParser = require('body-parser');
+	var express = require('express');
+	var http = require('http');
+	var app = express();
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({extended: false}));
+	app.use(cookieParser());
+	app.use(express.static(path.join(__dirname, 'public')));
+
+	var server = http.Server(app);
+	var io = require('socket.io')(server);
+
+	io.on('connection', function(socket)
+	{
+		console.log("socket.io connection handler...");
+		io.sockets.emit('message', { text: 'ok'});
+	});
+
+	server.listen(port, function() {
+		console.log('Express and socket.io listening on port ' + port);
+	});
+}
